1d6a1090a7b160f4a62f07001fbe1326e7f8a6ec
VishakAmin <aminvishak@gmail.com>
2021-06-15 18:17:57 +0530

    Added Food Ordering App

---
+++ Food_ordering/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Header from './components/Layout/Header';
+import Meals from './components/Meals/Meals';
+
+
+
+function App() {
+  return (
+    <React.Fragment>
+        <Header/>
+        <main>
+          <Meals/>
+        </main>
+    </React.Fragment>
+  );
+}
+
+export default App;
---
+++ Food_ordering/src/components/Layout/Header.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+import meal from "../../assets/meals.jpg"
+import classes from './Header.module.css'
+import HeaderCartButton from './HeaderCartButton'
+
+
+
+const Header = (props) => {
+  return <React.Fragment>
+      <header className={classes.header}>
+          <h1>
+            React Meals
+          </h1>
+        <HeaderCartButton/>
+      </header>
+      <div className={classes['main-image']}>
+        <img src={meal} alt="meal"/> 
+      </div>
+  </React.Fragment>
+}
+
+export default Header
