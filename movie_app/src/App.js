d663a979ca701bd74befccb9f55b7c6943b4af2c
Vishak Amin <aminvishak@gmail.com>
2021-06-17 18:16:37 +0530

    Added movie app

--- movie_app/src/App.js
+++ movie_app/src/App.js
@@ -1,31 +1,58 @@
-import React from 'react';
+import React,{useState, useEffect, useCallback} from 'react';
 
 import MoviesList from './components/MoviesList';
 import './App.css';
 
 function App() {
-  const dummyMovies = [
-    {
-      id: 1,
-      title: 'Some Dummy Movie',
-      openingText: 'This is the opening text of the movie',
-      releaseDate: '2021-05-18',
-    },
-    {
-      id: 2,
-      title: 'Some Dummy Movie 2',
-      openingText: 'This is the second opening text of the movie',
-      releaseDate: '2021-05-19',
-    },
-  ];
+
+  const [movies, setMovies] = useState([])
+  const [isLoading, setLoading] = useState(false)
+  const [error, setError] = useState(null)
+  
+
+ const fetchMovieHandler  = useCallback(async () =>  {
+  setLoading(true)
+  setError(null)
+  try {
+    const response =  await fetch("https://swapi.dev/api/films")
+    if(!response.ok){
+      throw new Error("Something went wrong")
+    }
+
+
+    const data = await response.json()
+ 
+    const transformedMovies = data.results.map(movie => {
+      return{
+        id: movie.episode_id,
+        title: movie.title,
+        openingText: movie.opening_crawl,
+        releaseDate: movie.release_date
+      }
+    })
+    setMovies(transformedMovies)
+   
+   } catch(error){
+      setError(error.message)
+  }
+  setLoading(false)
+}, [])
+
+useEffect(() => {
+  fetchMovieHandler()
+},[fetchMovieHandler])
+
 
   return (
     <React.Fragment>
       <section>
-        <button>Fetch Movies</button>
+        <button onClick={fetchMovieHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={dummyMovies} />
+        {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
+        {!isLoading&& movies.length === 0  &&<p>No movies</p>}
+        {isLoading&& <p>Loading.....</p>}
+        {!isLoading && error && <p>{error}</p> }
       </section>
     </React.Fragment>
   );
