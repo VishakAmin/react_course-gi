402e6317ea2a3673f4ea147d1c225df03b3a0147
Vishak Amin <aminvishak@gmail.com>
2021-06-18 16:36:44 +0530

    Added Form App

--- Form_app/src/App.js
+++ Form_app/src/App.js
@@ -1,9 +1,13 @@
+
+import BasicForm from './components/BasicForm';
 import SimpleInput from './components/SimpleInput';
+
 
 function App() {
   return (
     <div className="app">
-      <SimpleInput />
+      {/* <SimpleInput /> */}
+      <BasicForm/>
     </div>
   );
 }
