eb427521d44f30645a3c3b33bf57924389971c8e
Vishak Amin <aminvishak@gmail.com>
2021-06-21 23:43:02 +0530

    Completed Counter app with React Toolkit

---
+++ Counter_Redux_app/src/components/Counter.js
@@ -0,0 +1,48 @@
+import { useSelector, useDispatch } from 'react-redux'
+import { counterActions } from '../store';
+
+import classes from './Counter.module.css';
+
+
+
+const Counter = () => {
+  const dispatch = useDispatch()
+  const counter = useSelector(state => state.counter)
+  const show = useSelector(state => state.showCounter)
+
+  const toggleCounterHandler = () => {
+    dispatch(counterActions.toggleCounter())
+
+  };
+
+  const incrementHandler = () => {
+    dispatch(counterActions.increment())
+
+  }
+  const decrementHandler = () => {
+    dispatch(counterActions.decrement())
+  }
+  
+  const incrementBy5Handler = () => {
+    dispatch(counterActions.increase(5))
+
+  }
+
+
+
+  return (
+    <main className={classes.counter}>
+      <h1>Redux Counter</h1>
+      {show &&   <div className={classes.value}>{counter}</div>}
+      <div>
+        <button onClick={incrementHandler}>Increment</button>
+        <button onClick={incrementBy5Handler}>Increment By 5</button>
+        <button onClick={decrementHandler}>Decrement</button>
+      </div>
+      <button onClick={toggleCounterHandler}>Toggle Counter</button>
+
+      </main>
+  );
+};
+
+export default Counter;
